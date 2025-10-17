ef3067ee639e757cd80fe8b8765fae6f3eaa94e9
mgmaxi <maximiliano.ghirardi@gmail.com>
2025-10-16 21:55:30 -0300

    Fixs en build para deploy en Vercel

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,13 +1,15 @@
+import { Link } from "lucide-react";
+
 export default function NotFound() {
   return (
     <main className="flex flex-col items-center justify-center h-screen text-center">
       <p className="text-gray-600 mb-6">Parece que esta página no existe</p>
-      <a
+      <Link
         href="/"
         className="text-indigo-600 font-semibold hover:underline"
       >
         Ir a la página principal
-      </a>
+      </Link>
     </main>
   );
 }
